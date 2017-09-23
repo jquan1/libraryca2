af3397d40dbcf2a78f3411a041b32321f91c8265
JY <JY@DESKTOP-N1DGA2I>
2017-09-23 14:57:29 +0800

    commit changes 2:57pm

--- LibraryForm/Return.Designer.cs
+++ LibraryForm/Return.Designer.cs
@@ -31,7 +31,7 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBoxBookID = new System.Windows.Forms.TextBox();
             this.llBookTitle = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -58,113 +58,128 @@
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(435, 83);
+            this.dateTimePicker2.Location = new System.Drawing.Point(580, 102);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker2.TabIndex = 88;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(435, 44);
+            this.dateTimePicker1.Location = new System.Drawing.Point(580, 54);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 89;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(376, 86);
+            this.label7.Location = new System.Drawing.Point(501, 106);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.Size = new System.Drawing.Size(66, 17);
             this.label7.TabIndex = 86;
             this.label7.Text = "Due date";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(376, 49);
+            this.label8.Location = new System.Drawing.Point(501, 60);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.Size = new System.Drawing.Size(73, 17);
             this.label8.TabIndex = 87;
             this.label8.Text = "Issue date";
             // 
-            // textBox1
+            // TxtBoxBookID
             // 
-            this.textBox1.Location = new System.Drawing.Point(122, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(209, 20);
-            this.textBox1.TabIndex = 84;
+            this.TxtBoxBookID.Location = new System.Drawing.Point(163, 54);
+            this.TxtBoxBookID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtBoxBookID.Name = "TxtBoxBookID";
+            this.TxtBoxBookID.Size = new System.Drawing.Size(277, 22);
+            this.TxtBoxBookID.TabIndex = 84;
+            this.TxtBoxBookID.TextChanged += new System.EventHandler(this.TxtBoxBookID_TextChanged);
             // 
             // llBookTitle
             // 
             this.llBookTitle.AutoSize = true;
-            this.llBookTitle.Location = new System.Drawing.Point(30, 47);
+            this.llBookTitle.Location = new System.Drawing.Point(40, 58);
+            this.llBookTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llBookTitle.Name = "llBookTitle";
-            this.llBookTitle.Size = new System.Drawing.Size(46, 13);
+            this.llBookTitle.Size = new System.Drawing.Size(57, 17);
             this.llBookTitle.TabIndex = 82;
             this.llBookTitle.Text = "Book ID";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(122, 126);
+            this.textBox4.Location = new System.Drawing.Point(163, 155);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(209, 20);
+            this.textBox4.Size = new System.Drawing.Size(277, 22);
             this.textBox4.TabIndex = 92;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 130);
+            this.label4.Location = new System.Drawing.Point(40, 160);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.Size = new System.Drawing.Size(98, 17);
             this.label4.TabIndex = 90;
             this.label4.Text = "Member name";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(122, 86);
+            this.textBox5.Location = new System.Drawing.Point(163, 106);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(209, 20);
+            this.textBox5.Size = new System.Drawing.Size(277, 22);
             this.textBox5.TabIndex = 93;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 90);
+            this.label5.Location = new System.Drawing.Point(40, 111);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.Size = new System.Drawing.Size(76, 17);
             this.label5.TabIndex = 91;
             this.label5.Text = "Member ID";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 182);
+            this.dataGridView1.Location = new System.Drawing.Point(44, 224);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(298, 144);
+            this.dataGridView1.Size = new System.Drawing.Size(397, 177);
             this.dataGridView1.TabIndex = 94;
             // 
             // dateTimePicker3
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(435, 123);
+            this.dateTimePicker3.Location = new System.Drawing.Point(580, 151);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker3.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker3.TabIndex = 96;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(376, 126);
+            this.label1.Location = new System.Drawing.Point(501, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(83, 17);
             this.label1.TabIndex = 95;
             this.label1.Text = "Return date";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(255, 153);
+            this.button1.Location = new System.Drawing.Point(340, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 97;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
@@ -173,17 +188,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 237);
+            this.label2.Location = new System.Drawing.Point(40, 292);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(337, 13);
+            this.label2.Size = new System.Drawing.Size(450, 17);
             this.label2.TabIndex = 98;
             this.label2.Text = "Notes: Member search by ID or Name will be appear here for selecting";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(560, 227);
+            this.button2.Location = new System.Drawing.Point(747, 279);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 97;
             this.button2.Text = "Return";
             this.button2.UseVisualStyleBackColor = true;
@@ -191,14 +208,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(122, 67);
+            this.label3.Location = new System.Drawing.Point(163, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.Size = new System.Drawing.Size(185, 17);
             this.label3.TabIndex = 99;
             this.label3.Text = "Name of book appears here";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.hiGuestToolStripMenuItem,
             this.searchBorrowToolStripMenuItem,
@@ -208,7 +227,8 @@
             this.reportsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(670, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(893, 28);
             this.menuStrip1.TabIndex = 100;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -217,50 +237,50 @@
             this.hiGuestToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.logInToolStripMenuItem});
             this.hiGuestToolStripMenuItem.Name = "hiGuestToolStripMenuItem";
-            this.hiGuestToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.hiGuestToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
             this.hiGuestToolStripMenuItem.Text = "Hi, Guest";
             // 
             // logInToolStripMenuItem
             // 
             this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
-            this.logInToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.logInToolStripMenuItem.Size = new System.Drawing.Size(125, 26);
             this.logInToolStripMenuItem.Text = "Log in";
             // 
             // searchBorrowToolStripMenuItem
             // 
             this.searchBorrowToolStripMenuItem.Name = "searchBorrowToolStripMenuItem";
-            this.searchBorrowToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.searchBorrowToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
             this.searchBorrowToolStripMenuItem.Text = "Search & Borrow";
             // 
             // memberInformationToolStripMenuItem
             // 
             this.memberInformationToolStripMenuItem.Name = "memberInformationToolStripMenuItem";
-            this.memberInformationToolStripMenuItem.Size = new System.Drawing.Size(130, 20);
+            this.memberInformationToolStripMenuItem.Size = new System.Drawing.Size(159, 24);
             this.memberInformationToolStripMenuItem.Text = "Member Information";
             // 
             // bookInformationToolStripMenuItem
             // 
             this.bookInformationToolStripMenuItem.Name = "bookInformationToolStripMenuItem";
-            this.bookInformationToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.bookInformationToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
             this.bookInformationToolStripMenuItem.Text = "Book Information";
             // 
             // returnBookToolStripMenuItem
             // 
             this.returnBookToolStripMenuItem.Name = "returnBookToolStripMenuItem";
-            this.returnBookToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.returnBookToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
             this.returnBookToolStripMenuItem.Text = "Return book";
             // 
             // reportsToolStripMenuItem
             // 
             this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
-            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
             this.reportsToolStripMenuItem.Text = "Reports";
             // 
             // Return
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 396);
+            this.ClientSize = new System.Drawing.Size(893, 487);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -277,8 +297,9 @@
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtBoxBookID);
             this.Controls.Add(this.llBookTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Return";
             this.Text = "Return";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -295,7 +316,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtBoxBookID;
         private System.Windows.Forms.Label llBookTitle;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
