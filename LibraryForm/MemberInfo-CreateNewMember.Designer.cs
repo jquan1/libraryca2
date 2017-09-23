af5235dcd08947fb7d4084469fb2bf6aa68a8e63
nathalie1107 <nathalie.nhu.y@gmail.com>
2017-09-23 14:49:38 +0800

    Updated

--- LibraryForm/MemberInfo-CreateNewMember.Designer.cs
+++ LibraryForm/MemberInfo-CreateNewMember.Designer.cs
@@ -52,7 +52,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(245, 220);
+            this.button1.Location = new System.Drawing.Point(253, 218);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 23);
             this.button1.TabIndex = 35;
